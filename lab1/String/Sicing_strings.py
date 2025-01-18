40ce5693c836bb0b4372daf4b745fb221afad439
Olzhas <o_kuzembayev@kbtu.kz>
2025-01-18 23:58:33 +0500

    added

---
+++ lab1/String/Sicing_strings.py
@@ -0,0 +1,17 @@
+b = "Hello, World!"
+print(b[2:5])
+
+b = "Hello, World!"
+print(b[:5])
+
+# Negative Indexing
+# Use negative indexes to start the slice from the end of the string:
+# Example
+# Get the characters:
+
+# From: "o" in "World!" (position -5)
+
+# To, but not included: "d" in "World!" (position -2):
+
+b = "Hello, World!"
+print(b[-5:-2])
