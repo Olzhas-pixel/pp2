d07ed9c79a6af4b4837e4d09e523a7fed5474f8f
Olzhas <o_kuzembayev@kbtu.kz>
2025-01-26 10:20:27 +0500

    lab2

---
+++ lab2/PythonOperators.py
@@ -0,0 +1,43 @@
+Python Arithmetic Operators
+Arithmetic operators are used with numeric values to perform common mathematical operations:
+# +	Addition	x + y	
+# -	Subtraction	x - y	
+# *	Multiplication	x * y	
+# /	Division	x / y	
+# %	Modulus	x % y	
+# **	Exponentiation	x ** y	
+# //	Floor division	x // y
+
+Python Comparison Operators
+Comparison operators are used to compare two values:
+
+# ==	Equal	x == y	
+# !=	Not equal	x != y	
+# >	Greater than	x > y	
+# <	Less than	x < y	
+# >=	Greater than or equal to	x >= y	
+# <=	Less than or equal to	x <= y
+
+Python Bitwise Operators
+Bitwise operators are used to compare (binary) numbers:
+
+# & 	AND	Sets each bit to 1 if both bits are 1	x & y	
+# |	OR	Sets each bit to 1 if one of two bits is 1	x | y	
+# ^	XOR	Sets each bit to 1 if only one of two bits is 1	x ^ y	
+# ~	NOT	Inverts all the bits	~x	
+# <<	Zero fill left shift	Shift left by pushing zeros in from the right and let the leftmost bits fall off	x << 2	
+# >>	Signed right shift	Shift right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off	x >> 2
+
+# ()	Parentheses	
+# **	Exponentiation	
+# +x  -x  ~x	Unary plus, unary minus, and bitwise NOT	
+# *  /  //  %	Multiplication, division, floor division, and modulus	
+# +  -	Addition and subtraction	
+# <<  >>	Bitwise left and right shifts	
+# &	Bitwise AND	
+# ^	Bitwise XOR	
+# |	Bitwise OR	
+# ==  !=  >  >=  <  <=  is  is not  in  not in 	Comparisons, identity, and membership operators	
+# not	Logical NOT	
+# and	AND	
+# or	OR  
